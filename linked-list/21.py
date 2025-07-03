53ae701debf10da0489d3fa1e2f0df64b09c8718
giahuyyle <huyyyylegia@gmail.com>
2025-07-04 00:04:24 +0700

    created listnode class

--- linked-list/21.py
+++ linked-list/21.py
@@ -1,8 +1,6 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+from ListNode import ListNode
+from typing import Optional
+
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
         curr = ListNode()
